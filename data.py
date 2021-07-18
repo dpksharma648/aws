0e662f259a61f942fe4047f4cc231b38429f4018
dpksharma648 <87609837+dpksharma648@users.noreply.github.com>
2021-07-18 20:40:14 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 18 20:10:39 2021
+
+@author: dpksh
+"""
+import pandas as pd
+import seaborn as sns
+
+df=pd.read_csv(r"C:\Users\dpksh\Downloads\IMDB-Movie-Data.csv")
+
